3e63222241635d6a3c1396b53ca9def8ec687d4e
HANAI tohru <tohru@reedom.com>
2023-03-25 11:34:19 +0900

    feat: add PinnedLanguages, LanguagePreference models

--- src/models/preference.ts
+++ src/models/preference.ts
@@ -14,9 +14,22 @@ export interface VoicePreference {
   // provider specific voice options.
   voiceOptions?: any;
 }
+
 export interface GoogleVoiceOptions {
   speed?: number;
   pitch?: number;
   gender?: Gender;
   volume?: number;
+}
+
+export interface PinnedLanguages {
+  pinned: string[];
+  selected: string;
+}
+
+export interface LanguagePreference {
+  // language code. E.g. en, ja, etc.
+  lang: string;
+  // speed. E.g. 1.0, 1.5, etc.
+  speed: number;
 }
