cda9d967073da1e4feb14c025941cfa12a21ff51
HANAI tohru <tohru@reedom.com>
2023-03-25 11:33:39 +0900

    refactor: introduce strongly typed sendBackgroundMessage()

--- src/models/messages.ts
+++ src/models/messages.ts
@@ -1,9 +1,29 @@
-import { ProviderId } from './consts';
+import { ProviderVoice, VoiceProvider, VoiceProviderId } from './voiceProviders';
+import { VoicePreference } from './preference';
 
 export type BackgroundMessage =
   | { type: 'listProviders'}
-  | { type: 'listVoices', providerId: ProviderId, lang: string }
-  | { type: 'synthesize', providerId: ProviderId, voiceId: string, text: string }
+  | { type: 'listVoices', providerId: VoiceProviderId, lang: string }
+  | { type: 'synthesize', providerId: VoiceProviderId, voiceId: string, text: string }
   | { type: 'googleStoreApiKey', apiKey: string }
-  | { type: 'preference.setVoice', lang: string, providerId: ProviderId, voiceId: string }
+  | { type: 'preference.setVoice', lang: string, providerId: VoiceProviderId, voiceId: string }
   | { type: 'preference.listVoices' }
+
+type BackgroundMessageReturnType<T> =
+  T extends { type: 'listProviders' } ? VoiceProvider[] :
+  T extends { type: 'listVoices', providerId: VoiceProviderId, lang: string } ? ProviderVoice[] | string :
+  T extends { type: 'synthesize', providerId: VoiceProviderId, voiceId: string, text: string } ? string :
+  T extends { type: 'googleStoreApiKey', apiKey: string } ? string :
+  T extends { type: 'preference.setVoice', lang: string, providerId: VoiceProviderId, voiceId: string } ? string :
+  T extends { type: 'preference.listVoices' } ? VoicePreference[] :
+  never;
+
+export async function sendBackgroundMessage<M extends BackgroundMessage>(msg: M): Promise<BackgroundMessageReturnType<M>> {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.runtime.sendMessage(msg, resolve);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
