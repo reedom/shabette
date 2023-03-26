e2151f63372dd7d5f1c912ae377d9cb0b9239a06
HANAI tohru <tohru@reedom.com>
2023-03-26 11:18:12 +0900

    feat: add SelectableList component

---
+++ src/containers/SelectableList/SelectableList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as styles from './SelectableList.css';
+
+type Props = {
+  items: SelectableListItem[];
+  selected: string;
+}
+
+export type SelectableListItem = {
+  label: string;
+  value: string;
+  onClick: () => void;
+}
+
+export default function SelectableList(props: Props) {
+  const { items, selected } = props;
+  return (
+    <div className={styles.container}>
+      {items.map(item => (
+        <div
+          className={item.value === selected ? styles.selectedItem : styles.item}
+          onClick={item.value === selected ? undefined : item.onClick}
+        >{item.label}</div>
+      ))}
+    </div>
+  );
+}
