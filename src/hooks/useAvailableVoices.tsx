819c7a37e9a7fef6cb6b387173dee3e61946e3c5
HANAI tohru <tohru@reedom.com>
2023-03-26 16:06:49 +0900

    feat: add useAvailableVoices

---
+++ src/hooks/useAvailableVoices.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { listVoices } from '../models/backgroundMessages';
+import { ProviderVoice } from '../models/voiceProviders';
+import { useVoiceProviders } from './useVoiceProviders';
+
+type State = {
+  availableVoices: { [providerId: string]: ProviderVoice[] },
+}
+
+export function useAvailableVoices(): State {
+  const { voiceProviders } = useVoiceProviders();
+  const [availableVoices, setVoices] = useState<{ [providerId: string]: ProviderVoice[] }>({});
+
+  useEffect(() => {
+    if (!voiceProviders) return;
+    (async function() {
+      for (const provider of voiceProviders) {
+        const voices = await listVoices({ providerId: provider.id });
+        setVoices(prev => ({ ...prev, [provider.id]: voices }));
+      }
+    })();
+  }, [voiceProviders]);
+
+  return { availableVoices };
+}
