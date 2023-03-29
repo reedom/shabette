1f1de0422f9e1fd12b9e742e3bc5ae3099eb606f
HANAI tohru <tohru@reedom.com>
2023-03-29 21:19:26 +0900

    refactor

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,42 +1,8 @@
 import React from 'react';
-import { ProviderVoice, VoiceProvider } from '../../models/voiceProviders';
-import SelectableList from '../../containers/SelectableList/SelectableList';
-import { useVoiceProviders } from '../../hooks/useVoiceProviders';
-import { useAvailableVoices } from '../../hooks/useAvailableVoices';
-import { usePinnedVoices } from '../../hooks/usePinnedVoices';
-import { useSelectedVoice } from '../../hooks/useSelectedVoice';
-import VoiceItem from '../../containers/VoiceItem';
+import VoiceSelectionPanel from '../../containers/VoiceSelectionPanel';
 
 const Popup = () => {
-  const { voiceProviders, selectedVoiceProvider, selectVoiceProvider, isLoading: isVoiceProvidersLoading } =
-    useVoiceProviders();
-  const { availableVoices } = useAvailableVoices();
-  const { selectVoice, selectedVoice, isLoading: isLoadingSelectedVoice } = useSelectedVoice();
-  const { pinnedVoices, pinVoice, isLoading: isLoadingPinnedVoices } = usePinnedVoices();
-
-  if (isVoiceProvidersLoading || isLoadingSelectedVoice || isLoadingPinnedVoices ||
-    !selectedVoiceProvider || !availableVoices[selectedVoiceProvider?.id]) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <SelectableList
-        items={voiceProviders!.map((provider: VoiceProvider) => ({
-          label: provider.name,
-          value: provider.id,
-          onClick: () => selectVoiceProvider(provider.id),
-        }))}
-        selected={selectedVoiceProvider!.id}/>
-      {availableVoices[selectedVoiceProvider.id].map((voice: ProviderVoice) => (
-        <VoiceItem
-          {...voice}
-          selected={voice.voiceId === selectedVoice?.voiceId && voice.providerId === selectedVoice?.providerId}
-          onSelected={() => selectVoice(voice)}
-        />
-      ))}
-    </div>
-  );
+  return <VoiceSelectionPanel/>;
 };
 
 export default Popup;
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -5,21 +5,15 @@ import Popup from './Popup';
 import { VoiceProvidersProvider } from '../../hooks/useVoiceProviders';
 import { SelectedVoiceProvider } from '../../hooks/useSelectedVoice';
 import { PinnedVoicesProvider } from '../../hooks/usePinnedVoices';
-import { PinnedLangsProvider } from '../../hooks/usePinnedLangs';
-import { SelectedLangProvider } from '../../hooks/useSelectedLang';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <SelectedLangProvider>
-    <PinnedLangsProvider>
-      <VoiceProvidersProvider>
-        <SelectedVoiceProvider>
-          <PinnedVoicesProvider>
-            <Popup/>
-          </PinnedVoicesProvider>
-        </SelectedVoiceProvider>
-      </VoiceProvidersProvider>
-    </PinnedLangsProvider>
-  </SelectedLangProvider>
+  <VoiceProvidersProvider>
+    <SelectedVoiceProvider>
+      <PinnedVoicesProvider>
+        <Popup/>
+      </PinnedVoicesProvider>
+    </SelectedVoiceProvider>
+  </VoiceProvidersProvider>
 );
