612ea1183adae7a724ad8d0425b1ee715a2f1081
HANAI tohru <tohru@reedom.com>
2023-03-29 21:10:13 +0900

    feat: add LangsPanel

---
+++ src/containers/LangsPanel/LangsPanel.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as styles from './LangsPanel.css';
+import { usePinnedLangs } from '../../hooks/usePinnedLangs';
+import useSWR from 'swr';
+import { listLangs } from '../../models/backgroundMessages';
+import Chip from '../Chip';
+import { useSelectedLang } from '../../hooks/useSelectedLang';
+
+type Props = {}
+
+export type SelectableListItem = {
+  label: string;
+  value: string;
+  onClick: () => void;
+}
+
+export default function LangsPanel(props: Props) {
+  const { data: langs, ...langsState } = useSWR<string[]>('langs', listLangs);
+  const { selectedLang, selectLang, isLoading: isLoadingSelectedLang } = useSelectedLang();
+  const { pinnedLangs, pinLang, isLoading: isLoadingPinnedLangs } = usePinnedLangs();
+
+  console.log({ langs, pinnedLangs, selectedLang });
+
+  const isLoading = isLoadingSelectedLang || langsState.isLoading || isLoadingPinnedLangs;
+
+  return isLoading
+    ? <div className={styles.container}/>
+    : <div className={styles.container}>
+      {pinnedLangs!.map(lang => (
+        <Chip
+          label={lang}
+          color={lang === selectedLang ? 'primary' : 'default'} key={lang}
+          onClick={() => {
+            console.log('onClick');
+            selectLang(lang)
+          }}
+        />
+      ))}
+    </div>
+}
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -5,15 +5,21 @@ import Popup from './Popup';
 import { VoiceProvidersProvider } from '../../hooks/useVoiceProviders';
 import { SelectedVoiceProvider } from '../../hooks/useSelectedVoice';
 import { PinnedVoicesProvider } from '../../hooks/usePinnedVoices';
+import { PinnedLangsProvider } from '../../hooks/usePinnedLangs';
+import { SelectedLangProvider } from '../../hooks/useSelectedLang';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <VoiceProvidersProvider>
-    <SelectedVoiceProvider>
-      <PinnedVoicesProvider>
-        <Popup/>
-      </PinnedVoicesProvider>
-    </SelectedVoiceProvider>
-  </VoiceProvidersProvider>
+  <SelectedLangProvider>
+    <PinnedLangsProvider>
+      <VoiceProvidersProvider>
+        <SelectedVoiceProvider>
+          <PinnedVoicesProvider>
+            <Popup/>
+          </PinnedVoicesProvider>
+        </SelectedVoiceProvider>
+      </VoiceProvidersProvider>
+    </PinnedLangsProvider>
+  </SelectedLangProvider>
 );
